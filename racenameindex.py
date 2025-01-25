bf4c9e53b1877b2cab87f4d93c89b5aedcc13520
Shayane Katchera <45144374+shayanekat@users.noreply.github.com>
2025-01-25 08:10:15 +0100

    add dictionnary between race name and race index

---
+++ racenameindex.py
@@ -0,0 +1,75 @@
+class RaceNameIndex:
+    Australia = 1
+    Melbourne = 1
+    
+    China = 2
+    Shanghai = 2
+    
+    Japan = 3
+    Suzuka = 3
+    
+    Bahrain = 4
+    Sakhir = 4
+    
+    SaudiArabia = 5
+    Jeddah = 5
+    
+    Miami = 6
+    
+    Imola = 7
+    EmiliaRomagna = 7
+    
+    Monaco = 8
+    MonteCarlo = 8
+    
+    Spain = 9
+    Barcelona = 9
+    
+    Canada = 10
+    Montreal = 10
+    
+    Austria = 11
+    Spielberg = 11
+    RedBullRing = 11
+    RBR = 11
+    
+    UK = 12
+    Silverstone = 12
+    England = 12
+    
+    Belgium = 13
+    Spa = 13
+    
+    Hungary = 14
+    Budapest = 14
+    Hungaroring = 14
+    
+    Netherlands = 15
+    Zandvoort = 15
+    
+    Italy = 16
+    Monza = 16
+    
+    Azerbaijan = 17
+    Baku = 17
+    
+    Singapore = 18
+    
+    Austin = 19
+    
+    Mexico = 20
+    MexicoCity = 20
+    
+    Brazil = 21
+    SaoPaulo = 21
+    
+    LasVegas = 22
+    
+    Qatar = 23
+    Losail = 23
+    
+    AbuDhabi = 24
+    UAE = 24
+    YasMarina = 24
+    
+    
